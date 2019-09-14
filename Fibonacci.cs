37372a4f445ace6559b3af91a6d659b03b607f5a
AJ <ajay.20093@gmail.com>
2019-09-14 14:14:00 +0530

    Fibonacci
    For this question, you will write a program that generates values from the Fibonacci sequence. The Fibonnaci sequence is recursively defined by:
    
    Fn = Fn - 1 + Fn - 2
    
    Using the following seed values:
    
    F0 = 0, F1 = 1
    
    Given a number n, print the nth value of the Fibonacci sequence.

---
+++ Fibonacci.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+    
+         int M = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(M));
+    }
+     public static int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+}
